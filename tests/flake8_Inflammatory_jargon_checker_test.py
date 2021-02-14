52432ebdcc67be3eb7358977218c6894898bc4cc
Tom Goldberg <30830488+tomgold182@users.noreply.github.com>
2021-02-15 01:26:33 +0200

    tests working

--- tests/flake8_Inflammatory_jargon_checker_test.py
+++ tests/flake8_Inflammatory_jargon_checker_test.py
@@ -4,19 +4,20 @@ from flake8_Inflammatory_jargon_checker import InflammatoryJargonPlugin
 from typing import Set
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+TRIVAL_CASE_FILE_PATH = 'test files/clean_jargon_case.py'
+INFLAMMATORY_CASE_FILE_PATH = 'test files/Inflammatory_jargon_case.py'
 
-
-def _results(s: str) -> Set[str]:
-    tree = ast.parse(s)
-    plugin = InflammatoryJargonPlugin(tree=tree, filename=f'{ROOT_DIR}\\flake8_Inflammatory_jargon_checker_test.py')
+def _results(file_path:str) -> Set[str]:
+    tree = ast.parse("")
+    plugin = InflammatoryJargonPlugin(tree=tree, filename=f'{ROOT_DIR}\\{file_path}')
     return {f'{line}:{col} {msg}' for line, col, msg, _ in plugin.run()}
 
-def test_trivial_case():
-    assert _results('') == set()
+def test_clean_jargon_case():
+    assert _results(TRIVAL_CASE_FILE_PATH) == set()
 
-def test_inflammatory_jargon():
-    ret = _results('blacklist = {}')
-    assert ret == {f'1:1 IJU100 inflammatory jargon detected'}
+def test_inflammatory_jargon_case():
+    ret = _results(INFLAMMATORY_CASE_FILE_PATH)
+    assert ret == {"3:0 IJU100 inflammatory jargon detected,consider switching blacklist with blocklist"}
 
-def test_allowed_jargon():
-    assert _results('blocklist={}') == set()
+# def test_allowed_jargon():
+#     assert _results('blocklist={}') == set()
